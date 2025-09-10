7498a7e5b64ebf25ad743101f7d892ae70a9bee0
avishdilen <avish@livvittplus.com>
2025-09-10 12:28:31 -0400

    Delete livvitt-quote-invoice-demo directory

--- livvitt-quote-invoice-demo/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// If deploying to GitHub Pages under a repo path, set base to '/REPO_NAME/'
-export default defineConfig({
-  plugins: [react()],
-  // base: '/livvitt-quote-invoice-demo/'
-})
